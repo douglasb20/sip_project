33666710a367ec10fe9799fc4b8744ae856cb867
Douglas A. Silva <douglaassgenesis@gmail.com>
2023-06-21 19:31:29 -0300

    fiz para adicionar usuario

---
+++ App/Cron/baixa_callback_cron.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+class BaixaCallbackCron extends \Core\Defaults\DefaultCronController{
+
+    public function Baixa(){
+        try{
+            $cdr = new \App\Services\CdrService;
+            
+        }catch(\Exception $e){
+            throw $e;
+        }
+    }
+}
+
+try{
+    (new BaixaCallbackCron)->Baixa();
+}catch(\Exception $e){
+    throw $e;
+}
+
+?>
